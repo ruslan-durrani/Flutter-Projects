b08d65ae96e33c31549b095ac08a73d88aba8bc0
M Ruslan B <ruslandurrani907@gmail.com>
2023-10-29 20:20:27 +0500

    Miweth - Weather Application
    Personal Weather application. Where ever you go you get weather based on your location. Isn't it coool 🚀😁

---
+++ miweth/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()=>runApp(StartApp())
+
+class StartApp extends StatelessWidget {
+  const StartApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      ,
+    );
+  }
+}
