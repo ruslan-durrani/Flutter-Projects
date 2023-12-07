56ea571499d2b3459500161146bcea7e6f931512
M Ruslan B <ruslandurrani907@gmail.com>
2023-12-07 17:47:20 +0500

    Weekday update
    Update a file for get weekday from DateTime object and returning a string of weekday back to caller

--- pocket_pulse/lib/models/expense_data.dart
+++ pocket_pulse/lib/models/expense_data.dart
@@ -5,5 +5,16 @@ class ExpenseData{
   List<ExpenseItem> getListExpense() => listExpense;
   void addExpenseItem(ExpenseItem expenseItem) => listExpense.add(expenseItem);
   void removeExpenseItem(ExpenseItem expenseItem) => listExpense.remove(expenseItem);
-  // TODO get week days
+  String getWeekDay(){
+    switch(DateTime.now()){
+      case 1: return "Mon";
+      case 2: return "Tue";
+      case 3: return "Wed";
+      case 4: return "Thur";
+      case 5: return "Fri";
+      case 6: return "Sat";
+      case 7: return "Sun";
+      default: return "";
+    }
+  }
 }
