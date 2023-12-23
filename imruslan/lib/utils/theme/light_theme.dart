a367523a6bd02bd6a09cbba5e1500c197404896a
M Ruslan B <ruslandurrani907@gmail.com>
2023-12-23 22:17:42 +0500

    update to theme

--- imruslan/lib/utils/theme/light_theme.dart
+++ imruslan/lib/utils/theme/light_theme.dart
@@ -65,4 +65,102 @@ ThemeData lightTheme = ThemeData(
     )
 );
 
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Colors.orangeAccent.withOpacity(.1),
+        // background: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.black87
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      bodyColor: Colors.black87,
+      displayColor: Colors.black,
+    )
+);
+
+
  */
