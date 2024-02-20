6477a679b23fd7993c875e2d444b3a79d307abb8
M Ruslan B <ruslandurrani907@gmail.com>
2024-02-21 00:27:23 +0500

    update LostGet

---
+++ lost_get/lib/presentation_layer/screens/Home/components/quick_filter.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+class QuickFilterBar extends StatefulWidget {
+  final Function(int) onFilterSelected;
+
+  QuickFilterBar({Key? key, required this.onFilterSelected}) : super(key: key);
+
+  @override
+  _QuickFilterBarState createState() => _QuickFilterBarState();
+}
+
+class _QuickFilterBarState extends State<QuickFilterBar> {
+  int _selectedCategoryIndex = 0; // 'All' is selected by default
+  List<String> categories = ['All', 'Mobile', 'Cars', 'Wallet'];
+
+  void _selectCategory(int index) {
+    setState(() {
+      _selectedCategoryIndex = index;
+    });
+    widget.onFilterSelected(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.withOpacity(.2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/location_icon.svg'), // Replace with your location SVG asset
+            onPressed: () {
+              // TODO: Navigate to location filter
+            },
+          ),
+          // Wrap the ListView with a Container and give it a bounded height
+          Expanded(
+            child: Container(
+              height: 36.0, // Set a fixed height for the ListView
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: ChoiceChip(
+                      showCheckmark: false,
+
+                      label: Text(categories[index]),
+                      selected: _selectedCategoryIndex == index,
+                      onSelected: (_) => _selectCategory(index),
+                      selectedColor: Theme.of(context).colorScheme.primary,
+                      labelStyle: TextStyle(
+                        color: _selectedCategoryIndex == index ? Colors.white : Colors.black,
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.background,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/filter_icon.svg'), // Replace with your filter SVG asset
+            onPressed: () {
+              // TODO: Navigate to filter bottomSheet
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lost_get/lib/presentation_layer/screens/Home/home_screen.dart
+++ lost_get/lib/presentation_layer/screens/Home/home_screen.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lost_get/models/user_profile.dart';
 import 'package:lost_get/services/chat_system_services/chat_service.dart';
 
 import '../../widgets/my_user_component.dart';
 import '../ChatBot/chatbot_screen.dart';
 import '../Messenger/chat_screen.dart';
+import 'components/quick_filter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,15 +21,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   final ChatService _chatService = ChatService();
-  // final AuthService _authService = AuthService();
+  int _selectedFilterIndex = 0; // The index of the selected filter
+
+  // Define a method to handle filter change
+  void _handleFilterChange(int index) {
+    setState(() {
+      _selectedFilterIndex = index;
+    });
+    // You can also fetch data based on the selected filter here
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: null,
-        title: Text("Users"),
+        title: SvgPicture.asset('assets/icons/lostget_logo.svg'), // Replace with your SVG file path
+        actions: <Widget>[
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/search_icon.svg'), // Replace with your SVG file path
+            onPressed: () {
+              // Action for search icon
+            },
+          ),
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/notification_icon.svg'), // Replace with your SVG file path
+            onPressed: () {
+              // Action for notification icon
+            },
+          ),
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/scan_qr_icon.svg'), // Replace with your SVG file path
+            onPressed: () {
+              // Action for profile icon
+            },
+          ),
+        ],
       ),
-      body: _buildUser(),
+      body: Column(
+        children: [
+          QuickFilterBar(
+            onFilterSelected: _handleFilterChange, // Pass the filter change handler
+          ),
+          Expanded(
+            child: _buildBodyContent(), // Build the body content based on the selected filter
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         onPressed:() => Navigator.pushNamed(context, ChatBotScreen.routeName),
@@ -38,7 +76,40 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-  Widget _buildUser(){
+
+  Widget _buildBodyContent() {
+    // Based on the selected filter index, return different Widgets/content
+    switch (_selectedFilterIndex) {
+      case 0:
+      // This is just an example, replace with your actual content for 'All'
+        return Center(child: Text('All selected'));
+      case 1:
+      // Replace with your actual content for 'Mobile'
+        return Center(child: Text('Mobile selected'));
+      case 2:
+      // Replace with your actual content for 'Cars'
+        return Center(child: Text('Cars selected'));
+      case 3:
+      // Replace with your actual content for 'Wallet'
+        return Center(child: Text('Wallet selected'));
+      default:
+        return Center(child: Text('Content not available'));
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
+Widget _buildUser(){
     return StreamBuilder(stream: _chatService.getUserStream(), builder: (context,snapshot){
       if(snapshot.hasError){
         return ScaffoldMessenger(child: Container(child: Text("Error Occured"),));
@@ -76,4 +147,4 @@ class _HomeScreenState extends State<HomeScreen> {
       );
     });
   }
-}
+ */
